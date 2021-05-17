236295897b650f9dc08fe80b57ccf1119ba3edde
Alex Wade <38112687+adw62@users.noreply.github.com>
2021-05-17 16:17:08 +0100

    First modular draft

---
+++ ties_analysis/TIES_analysis.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import os
+
+from config import Config
+import numpy as np
+
+class Analysis():
+    def __init__(self):
+
+        cfg = Config()
+        result = {}
+
+        for engine in cfg.engines:
+            engine_id = engine.name+'_'+engine.method
+            if engine_id not in result:
+                result[engine_id] = {}
+            else:
+                raise ValueError('Repeated engine name {}'.format(engine_id))
+
+            for prot_name, prot_data in cfg.exp_data.items():
+                if prot_name not in result[engine_id]:
+                    result[engine_id][prot_name] = {}
+                else:
+                    raise ValueError('Repeated protein name {}'.format(prot_name))
+
+                for lig_name, lig_data in prot_data.items():
+                    if lig_name not in result[engine_id][prot_name]:
+                        result[engine_id][prot_name][lig_name] = {}
+                    else:
+                        raise ValueError('Repeated ligand name {}'.format(lig_name))
+
+                    nice_print('{} {} {}'.format(engine_id, prot_name, lig_name))
+                    leg_results = {leg: 0.00 for leg in cfg.simulation_legs}
+                    for leg in cfg.simulation_legs:
+                        nice_print(leg)
+                        leg_results[leg] = engine.run_analysis(cfg.data_root, prot_name, lig_name, leg)
+                        print('dG = {}: err = {}'.format(*leg_results[leg]))
+
+                    ddg = leg_results['lig'][0] - leg_results['com'][0]
+                    ddg_err = np.sqrt(np.square(leg_results['lig'][1]) + np.square(leg_results['com'][1]))
+                    print('ddG = {}: err = {}'.format(ddg, ddg_err))
+                    result[engine_id][prot_name][lig_name] = [ddg, ddg_err]
+
+def nice_print(string):
+    '''
+
+    :param string: str to format
+    :return: str, formatted string
+    '''
+    string = string.center(75, '#')
+    print(string)
+
+
+if __name__ == '__main__':
+    nice_print('Analysis')
+
+    if os.path.exists('./all_res.dat'):
+        result = eval(open('./all_res.dat').read())
+    else:
+        result = {}
+
+    ana = Analysis()
---
+++ ties_analysis/config.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+from engines.openmm import OpenMM
+from engines.namd import NAMD
+
+
+class Config():
+    def __init__(self, namd_cfg='./namd.cfg', openmm_cfg='./openmm.cfg', analysis_cfg='./analysis.cfg'):
+        '''
+
+        :param namd_cfg: str, path to file containing namd analysis params
+        :param openmm_cfg: path to file containing openmm analysis params
+        :param analysis_cfg: path to file containing general analysis params
+        '''
+
+        # process general arg for analysis
+        # general args specify where data is located what methods to use and any other parameters
+        general_args = read_config(analysis_cfg)
+
+        self.engines_to_init = general_args['engines']
+        self.simulation_legs = general_args['legs']
+        self.data_root = general_args['data_root'][0]
+        self.analysis_dir = general_args['output_dir'][0]
+
+        # what alchemical windows to remove from analysis
+        if general_args['windows_mask'][0] != 'None':
+            self.win_mask = [int(x) for x in general_args['windows_mask']]
+        else:
+            self.win_mask = None
+
+        # Process engines
+        self.engines = []
+        if 'NAMD2' in self.engines_to_init or 'NAMD3' in self.engines_to_init:
+            namd_args = read_config(namd_cfg)
+            methods = namd_args['methods']
+            del namd_args['methods']
+            for method in methods:
+                namd = NAMD(method, self.analysis_dir, self.win_mask, **namd_args)
+                self.engines.append(namd)
+
+        if 'OpenMM' in self.engines_to_init:
+            openmm_args = read_config(openmm_cfg)
+            methods = openmm_args['methods']
+            del openmm_args['methods']
+            for method in methods:
+                openmm = OpenMM(method, self.analysis_dir, self.win_mask, **openmm_args)
+                self.engines.append(openmm)
+
+        #process exp data
+        # EXP data specifies what proteins and ligands to look at
+        self.exp_data = eval(open(general_args['exp_data'][0]).read())
+
+
+def read_config(config_file):
+    '''
+
+    :param config_file: str, path to a config file
+    :return: dict, containing all options to run analysis
+    '''
+    args_dict = {}
+    with open(config_file) as file:
+        for line in file:
+            if line[0] != '#' and line[0] != ' ' and line[0] != '\n':
+                data = line.rstrip('\n').split('=')
+                if len(data) > 2:
+                    raise ValueError('Failed to parse line: {}'.format(line))
+                # Remove spaces
+                data = [s.replace(" ", "") for s in data]
+                data = [s.replace("\t", "") for s in data]
+                args_dict[data[0]] = data[1]
+    args_dict = {k: v.split(',') for k, v in args_dict.items()}
+
+    return args_dict
